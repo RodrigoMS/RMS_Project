f99f185241ff1dd915b44a425086658f1883926d
RodrigoMS <informaticarodrigomessias@gmail.com>
2022-12-12 15:01:49 -0300

    Update 12122022

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# RMS_Project
+# Olá! 
+
+Estou criando meu site pessoal. Intitulado "RMS_Project"
+
+Veja como está ficando, [RMS Project](https://rodrigoms.github.io/RMS_Project/). 
---
+++ assets/js/create_hexagono.js
@@ -0,0 +1,52 @@
+const light = document.querySelector(".light")
+const background = document.querySelector(".background-hexagono")
+
+background.addEventListener("mousemove", (e) => {
+  light.style.left = `${(e.clientX) - 150}px`
+  light.style.top = `${(e.clientY) - 150 }px`
+})
+
+// Cria os hexágonos.
+function createHexagons() {
+  let exchange = true
+  let topValue = 42
+
+  let back = `<div id="container0" class="container-hexagono position-2" style="top: -50px">`
+
+  for (let index = 1; index < 49; index++) {
+
+    if (exchange) {
+      back += `<div id="container${index}" class="container-hexagono" style="top: ${topValue}px">`
+      exchange = false
+  
+    } else {
+      back += `<div id="container${index}" class="container-hexagono position-2" style="top: ${topValue}px">`
+      exchange = true
+    }
+
+    topValue += 92
+
+    for (let index = 0; index < 77; index++) {
+      back += `<div class="hexagono"></div>`
+    }
+
+    back +=`</div>`
+  }
+
+  //background.innerHTML += back
+
+  const hexagonosWallpaper = document.createElement("div")
+  hexagonosWallpaper.innerHTML = back
+  background.appendChild(hexagonosWallpaper)
+}
+
+createHexagons()
+
+//Troca a cor dos hexágonos a cada 5 segundos.
+/*setInterval(() => {
+
+
+  let hexagon = document.querySelector("#light")
+
+
+}, 5000);*/
